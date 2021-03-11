80a8a1ff6148f3aeae51fe08042f937e321c6053
philiplee25 <yplunap25@gmail.com>
2021-03-11 12:01:34 +0900

    .

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Set;
 import com.eomcs.pms.table.BoardTable;
 import com.eomcs.pms.table.DataTable;
+import com.eomcs.pms.table.MemberTable;
+import com.eomcs.pms.table.ProjectTable;
 import com.eomcs.util.Request;
 import com.eomcs.util.Response;
 
@@ -32,6 +34,8 @@ public class ServerApp {
 
     // 요청을 처리할 테이블 객체를 준비한다.
     tableMap.put("board/", new BoardTable());
+    tableMap.put("member/", new MemberTable());
+    tableMap.put("project/", new ProjectTable());
 
     // 클라이언트 연결을 기다는 서버 소켓 생성
     try (ServerSocket serverSocket = new ServerSocket(this.port)) {
