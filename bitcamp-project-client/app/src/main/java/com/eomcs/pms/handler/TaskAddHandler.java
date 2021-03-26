61af5b980f32a0b7c7189eefb74dd1c8d9f33b86
philiplee25 <yplunap25@gmail.com>
2021-03-26 11:12:22 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
@@ -1,16 +1,18 @@
 package com.eomcs.pms.handler;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import com.eomcs.driver.Statement;
+import com.eomcs.pms.domain.Project;
 import com.eomcs.pms.domain.Task;
 import com.eomcs.util.Prompt;
 
 public class TaskAddHandler implements Command {
 
-  Statement stmt;
   MemberValidator memberValidator;
 
-  public TaskAddHandler(Statement stmt, MemberValidator memberValidator) {
-    this.stmt = stmt;
+  public TaskAddHandler(MemberValidator memberValidator) {
     this.memberValidator = memberValidator;
   }
 
@@ -19,6 +21,7 @@ public class TaskAddHandler implements Command {
   public void service() throws Exception {
     System.out.println("[작업 등록]");
 
+    
     Task t = new Task();
     t.setContent(Prompt.inputString("내용? "));
     t.setDeadline(Prompt.inputDate("마감일? "));
@@ -30,13 +33,19 @@ public class TaskAddHandler implements Command {
       return;
     }
 
-    stmt.executeUpdate("task/insert", 
-        String.format("%s,%s,%s,%s", 
-            t.getContent(),
-            t.getDeadline(),
-            t.getStatus(),
-            t.getOwner()));
+    try (Connection con = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+        PreparedStatement stmt = con.prepareStatement(
+            "insert into pms_task(content,deadline,status,owner)"
+                + " values(?,?,?,?)");) {
 
-    System.out.println("작업을 등록했습니다.");
+      stmt.setString(1, t.getContent());
+      stmt.setDate(2, t.getDeadline());
+      stmt.setInt(3, t.getStatus());
+      stmt.setString(4, t.getOwner());
+      stmt.executeUpdate();
+
+      System.out.println("프로젝트를 등록했습니다.");
+    }
   }
 }
